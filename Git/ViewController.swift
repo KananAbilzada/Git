e43bd09a66723b0048c9b21a15ed599da7c7f8df
Kanan Abilzada <kenanabilzade27@gmail.com>
2020-08-21 08:35:22 +0400

    Initial Commit

---
+++ Git/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Git
+//
+//  Created by Kanan`s Macbook Pro on 8/21/20.
+//  Copyright © 2020 Kanan`s Macbook Pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
